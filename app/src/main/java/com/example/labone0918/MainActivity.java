ff093c124cde4b5078c501691a5245821143a840
keistoteka <ausrine.cicinskaite@gmail.com>
2023-10-09 12:52:33 +0300

    Added change text functionality

--- app/src/main/java/com/example/labone0918/MainActivity.java
+++ app/src/main/java/com/example/labone0918/MainActivity.java
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain=findViewById(R.id.tvMain);
     }
 
     public void onButtonChangeTextClick(View view) {
+
+        this.tvMain.setText("Change text");
+
         Toast.makeText(this, "Some msg", Toast.LENGTH_LONG).show();
     }
 }
